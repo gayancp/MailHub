65e3c5e10deb998680d277255648191070a42180
gayancp <128348582+gayancp@users.noreply.github.com>
2026-06-13 23:43:21 +0530

    initial commit

---
+++ bakend/mailhub/src/main/java/com/example/gayanchinthaka/mailhub/backend/MailhubApplication.java
@@ -0,0 +1,13 @@
+package com.example.gayanchinthaka.mailhub.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailhubApplication.class, args);
+	}
+
+}
---
+++ bakend/mailhub/src/test/java/com/example/gayanchinthaka/mailhub/backend/MailhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gayanchinthaka.mailhub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
